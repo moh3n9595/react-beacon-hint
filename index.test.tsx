81581b434953457f4ab9bb66919386987cff520b
Mohsen <mohsenando@gmail.com>
2023-01-01 01:46:37 +0330

    chore: add json deps & remove extra tests

--- index.test.tsx
+++
@@ -1,9 +0,0 @@
-import {Fragment} from 'react';
-import {render} from './src/test/utils';
-
-describe('Simple working test', () => {
-	it('the title is visible', () => {
-		render(<Fragment />);
-		expect(true).toBe(true);
-	});
-});
