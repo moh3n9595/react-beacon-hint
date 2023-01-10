89328c5e9fbdeac50eba608960cd4ffe0757a70e
Mahdi Ash Archin <iarchin95@gmail.com>
2023-01-10 22:43:49 +0300

    feat: change pallette, add code block

--- example/src/screens/HomePage/Home.screen.tsx
+++ example/src/screens/HomePage/Home.screen.tsx
@@ -1,11 +1,17 @@
 import {memo} from 'react';
-// import './MainLayout.scss';
-import {OutlineBeacon} from '../../../../index';
+import './Home.scss';
+import {Hint, OutlineBeacon} from '../../../../index';
 const Home = () => {
 	return (
 		<>
-			<OutlineBeacon size={160} />
+			<OutlineBeacon size={180} color='#c7d4f2' />
 			<h1>Welcome To React Beacon Hint</h1>
+			<p>here is a very basic example just click on it and see what happens! </p>
+			<div className='result-container'>
+				<Hint popover='Hello from hint!' beaconProps={{placement: 'right'}}>
+					<button>This needs a hint</button>
+				</Hint>
+			</div>
 		</>
 	);
 };
