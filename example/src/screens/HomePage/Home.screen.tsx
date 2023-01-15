da286cb25e5ef43e2ccd758e5a0cbaffad949396
Mahdi Ash Archin <iarchin95@gmail.com>
2023-01-15 20:06:46 +0300

    fix: better bottom wave, add more tailwind for styling, add beacon

--- example/src/screens/HomePage/Home.screen.tsx
+++ example/src/screens/HomePage/Home.screen.tsx
@@ -4,20 +4,16 @@ import {Hint} from '../../../../index';
 import {CodeSnippet} from '../../components';
 import {quickStartSnippet} from '../../codeSnippets';
 import {Wave} from './wave';
+import {Beacon} from '../../assets/svgs';
+import {FillBeacon} from 'react-beacon-hint';
 const Home = () => {
 	return (
 		<>
 			<div className='w-3/4 flex flex-col items-center justify-center'>
-				{/* <>
-					<OutlineBeacon size={40} className='beacon' />
-					<div className='flex justify-center'>
-						<Beacon size={200} fill='#c7d4f2' />
-					</div>
-				</> */}
-				<h1 className='text-6xl'>React Beacon Hint</h1>
-				<p>User Onboarding Component for React with Fully Configurable Options!</p>
-				<div className='w-full flex flex-row justify-around mt-10'>
-					<div className='result-container'>
+				<h1 className='text-5xl md:text-7xl text-zinc-100 mb-5 '>React Beacon Hint</h1>
+				<p className='text-2xl mb-10'>User Onboarding Component for React with Fully Configurable Options!</p>
+				<div className='w-full flex flex-col-reverse xl:flex-row justify-around items-center mt-10'>
+					<div className='result-container mt-5 xl:mt-0'>
 						<Hint popover='Yay! I Appeared!'>
 							<button className='sample-button'>Click The Hint</button>
 						</Hint>
@@ -25,8 +21,9 @@ const Home = () => {
 					<CodeSnippet language='javascript' code={quickStartSnippet} />
 				</div>
 			</div>
-			<div className='fixed inset-x-0 bottom-0 h-52'>
-				{/* <div className='footer-inner-container'>
+			<div className='fixed bottom-0 w-screen'>
+				<div className='w-full relative'>
+					{/* <div className='footer-inner-container'>
 					<div>
 						<code>&#62; npm i -S react-beacon-hint</code>
 					</div>
@@ -34,7 +31,12 @@ const Home = () => {
 						<code>&#62; yarn add -S react-beacon-hint</code>
 					</div>
 				</div> */}
-				<Wave />
+					<div className='absolute right-5 bottom-0 -z-10 w-1/6'>
+						<FillBeacon size={25} color='#FF5700' className='relative top-2' />
+						<Beacon fill='#fff' />
+					</div>
+					<Wave />
+				</div>
 			</div>
 		</>
 	);
