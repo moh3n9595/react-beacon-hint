db6b1ddf262223d33290870c5172579487f0119f
Mahdi Ash Archin <iarchin95@gmail.com>
2023-02-05 18:20:34 +0300

    fix: multiple size clouds, smoother move

--- example/src/components/Clouds/GenerateClouds.tsx
+++ example/src/components/Clouds/GenerateClouds.tsx
@@ -1,55 +1,86 @@
-import {motion, ResolvedValues} from 'framer-motion';
-import {memo, useEffect, useState} from 'react';
+import {motion} from 'framer-motion';
+import {memo, useCallback, useEffect, useState} from 'react';
 import {Cloud} from '../../assets/svgs';
 
 type ICloud = {
 	id: number;
 	top: number;
-	left: string;
+	left: number;
 	direction: string;
+	finished: boolean;
+	duration: number;
+	size: 'small' | 'medium' | 'large';
 };
 
 const CloudGenerator = () => {
 	const [clouds, setClouds] = useState<ICloud[]>([]);
-	// const MAX_MOVE_THRESHOLD = window.innerWidth;
+
+	const MAX_MOVE_THRESHOLD = window.innerWidth;
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			if (clouds.length >= 5) return;
-
+			if (clouds.length >= 4) return;
+			const RANDOM_NUM = Math.floor(1 + Math.random() * 3);
+			const cloudLeftPosition = Math.floor(1 + Math.random() * MAX_MOVE_THRESHOLD);
 			setClouds([
 				...clouds,
 				{
-					id: clouds.length + 1,
+					id: Math.random() * 180,
 					top: Math.floor(1 + Math.random() * 180),
-					left: Math.floor(1 + Math.random() * 100) + '%',
-					direction: Math.random() >= 0.5 ? 'right' : 'left',
+					left: cloudLeftPosition,
+					direction: cloudLeftPosition < MAX_MOVE_THRESHOLD / 2 ? 'right' : 'left',
+					finished: false,
+					duration: Math.floor(20 + Math.random() * 20),
+					size: RANDOM_NUM === 1 ? 'small' : RANDOM_NUM === 2 ? 'medium' : 'large',
 				},
 			]);
 		}, 1000);
 
 		return () => clearInterval(interval);
-	}, [clouds]);
+	}, [MAX_MOVE_THRESHOLD, clouds]);
 
-	const onUpdate = (latest: ResolvedValues, cloud: ICloud) => {
-		if (latest.x >= 450 || latest.x <= -450) {
-			setClouds((prev) => prev.filter((c) => c.id !== cloud.id));
+	const removeCloud = useCallback(
+		(id: number) => {
+			const newArr = clouds.filter((cl) => cl.id !== id);
+			setClouds(newArr);
+		},
+		[clouds],
+	);
+
+	const cloudSize = useCallback((cl: string) => {
+		switch (cl) {
+			case 'small':
+				return 'w-24 h-24';
+			case 'medium':
+				return 'w-32 h-32';
+			case 'large':
+				return 'w-40 h-40';
+
+			default:
+				break;
 		}
-	};
+	}, []);
 
 	return (
-		<div className='clouds absolute w-full h-60 flex justify-between'>
+		<div className='clouds absolute w-full h-60 flex justify-between overflow-x-hidden'>
 			{clouds.map((cloud) => {
 				return (
 					<motion.div
 						key={cloud.id}
-						animate={{x: cloud.direction === 'right' ? 500 : -500}}
-						transition={{
-							duration: 15,
-							// delay: 1.8,
+						initial={{opacity: 0}}
+						animate={{
+							x: cloud.direction === 'left' ? cloud.left - MAX_MOVE_THRESHOLD : -(cloud.left - MAX_MOVE_THRESHOLD),
+							opacity: [0, 0.8, 0],
 						}}
-						onUpdate={(latest) => onUpdate(latest, cloud)}
-						className={`w-24 h-24 absolute z-10`}
+						transition={{
+							duration: cloud.duration,
+							times: [0, 0.5, 1],
+							loop: Infinity,
+							ease: 'easeInOut',
+						}}
+						onAnimationComplete={() => removeCloud(cloud.id)}
+						// onUpdate={(latest) => onUpdate(latest, cloud)}
+						className={`${cloudSize(cloud.size)} absolute z-10`}
 						style={{left: cloud.left, top: cloud.top}}
 					>
 						<Cloud />
@@ -62,5 +93,3 @@ const CloudGenerator = () => {
 
 const MemoizedCloudGenerator = memo(CloudGenerator);
 export {MemoizedCloudGenerator as CloudGenerator};
-
-// style={{top: Math.floor(5 + Math.random() * 180), left: Math.floor(20 + Math.random() * window.innerWidth)}}
