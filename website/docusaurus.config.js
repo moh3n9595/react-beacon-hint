ed0e2cfe2471829537279aa4e3200de07605448c
Mohsen <mohsenando@gmail.com>
2023-02-09 00:54:29 +0330

    chore: add doc url in config file

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -12,15 +12,10 @@ const config = {
 	favicon: '/img/favicon.ico',
 
 	// Set the production url of your site here
-	url: 'https://your-docusaurus-test-site.com',
+	url: 'https://react-beacon-hint-doc.vercel.app',
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: '/',
-
-	// GitHub pages deployment config.
-	// If you aren't using GitHub pages, you don't need these.
-	organizationName: 'facebook', // Usually your GitHub org/user name.
-	projectName: 'docusaurus', // Usually your repo name.
 
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
